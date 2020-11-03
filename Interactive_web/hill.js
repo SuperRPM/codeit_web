04c1cd13d079a2754c9ce8517cdfb4b16b43efd3
RPM <vidafirmar@gmail.com>
2020-11-03 17:36:55 +0900

    Modify hill.js but @@not complete@@

--- Interactive_web/hill.js
+++ Interactive_web/hill.js
@@ -9,13 +9,58 @@ export class Hill {
 		this.stageWidth - stageWidth;
 		this.stageHeight = stageHeight;
 
+		this.points = [];
+		this.gap = Math.ceil(this.stageWidth / (this.total -2));
+
+		for (let i = 0; i < this.total; i++) {
+			this.points[i] = {
+				x: i * this.gap,
+				y: this.getY()
+			};
+		}
+		
 	}
 
 	draw(ctx) {
+		ctx.fillStyle = this.color;
+		ctx.beginPath();
+
+		let cur = this.points[0];
+		let prev = cur;
+
+		let dots = [];
+
+		ctx.moveTo(cur.x, cur.y);
+
+		let prevCx = cur.x;
+		let prevCy = cur.y;
+
+		for (let i =1; i < this.points.length; i++) {
+			cur = this.points[i];
+
+			const cx = (prev.x +cur.x) / 2;
+			const cy = (prev.y + cur.y) / 2;
+			ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
+
+			dots.push({
+				x1: prevCx,
+				y1: prevCy,
+				x2: prev.x,
+				y2: prev.y,
+				x3: cx,
+				y3: cy,
+			});
+
+			prev = cur;
+			prevCx = cx;
+			prevCy = cy;
 	
 	}
 
 	getY() {
+		const min = this.stageHeight /8;
+		const max = this.stageHeight -min;
+		return min + Math.random() * max;
 
 	}
 
